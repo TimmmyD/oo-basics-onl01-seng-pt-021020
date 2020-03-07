b087f4cdf1692b2fdb8eb88803ce2ac0d0b41b50
Tim Donoghue <tmd112593@gmail.com>
2020-03-07 23:19:10 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,4 +16,23 @@ class Book
     @author 
   end
   
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre 
+  end
+  
+  def genre
+    @genre 
+    
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end 
+  
 end 
